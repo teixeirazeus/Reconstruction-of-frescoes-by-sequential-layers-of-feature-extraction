21a32228f92921d42245c2a40429c0946443d7f3
teixeirazeus <teixeira.zeus@gmail.com>
2021-04-26 14:52:32 -0300

    code

---
+++ prometeu.py
@@ -0,0 +1,233 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  prometeu.py
+#
+#  Copyright 2019 Thiago da Silva Teixeira <teixeira.zeus@gmail.com>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#
+
+
+import numpy as np
+import sys
+from threading import Thread
+import threading
+import os
+
+import cv2
+import time
+import random
+
+import multiprocessing
+import itertools as it
+
+
+from tools import *
+
+#os.chdir(os.path.dirname(sys.argv[0]))
+
+
+def assembler(frag_image, paint, heap, metodo, out, thread_id, threadLock):
+    kp2, des2 = paint
+    FLANN_INDEX_KDTREE = 0
+    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+    search_params = dict(checks = 100)
+
+    fragments = []
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+    end = False
+
+    while True:
+
+        threadLock.acquire()
+        if len(heap) > 0:
+            i = heap.pop()
+            #print("Sou o", thread_id,"peguei a peça",i)
+        else:
+            #print(thread_id, 'out')
+            end = True
+        threadLock.release()
+        if end: break
+
+        #for angle in range(0,360,360//16):
+        for angle in [0]:
+            try:
+                frag_rot = rotate(frag_image[i], angle)
+                size = max(frag_rot.shape)
+                frag_data = analise(frag_rot, metodo)
+            except:
+                print("Erro peça",i)
+                break
+
+            matches = flann.knnMatch(frag_data[1], des2, k=2)
+
+            matches = [m for m,n in matches if m.distance < 0.7*n.distance]
+
+            pontos = [[x.trainIdx, kp2[x.trainIdx].pt] for x in matches]
+            pontos = tri_min(pontos, size)
+            matches = [m for m in matches if m.trainIdx in pontos]
+
+            if len(matches) >= 3:
+                src_pts = np.float32([ frag_data[0][m.queryIdx].pt for m in matches ]).reshape(-1,1,2)
+                dst_pts = np.float32([ kp2[m.trainIdx].pt for m in matches ]).reshape(-1,1,2)
+                M = cv2.estimateRigidTransform(src_pts, dst_pts, fullAffine=False)
+                if M is not None:
+
+                    xfrag,yfrag = frag_image[i].shape
+                    xfrag /= 2; yfrag /= 2
+
+                    tx = M[0][0]*xfrag + M[0][1]*yfrag + M[0][2]
+                    ty = M[1][0]*xfrag + M[1][1]*yfrag + M[1][2]
+
+                    a = M[0][0]; c = M[0][1]
+                    b = M[1][0]; d = M[1][1]
+
+                    if np.isnan(a) or np.isnan(b):
+                        r = np.arctan2(c,d)
+                    else:
+                        r = np.arctan2(-b,a)
+
+                    r = np.degrees(r)
+
+                    print(thread_id, 'get', i, ':', metodo)
+
+                    rot = r+angle
+                    if rot > 360: rot -= 360
+                    if rot < -360: rot += 360
+
+                    fragments.append([M, angle, tx, ty, rot, i, dst_pts])
+
+                    break
+    out[thread_id] = fragments
+
+def main(args):
+
+    #target = 'Domenichino_Virgin-and-unicorn.jpg'
+    #target = 'Fra_Angelico_Cristoderiso_2536x3172.jpg'
+    target = 'target.jpg'
+    #target = input("Target:")
+
+    total = 0
+    for root, dirs, files in os.walk("./frag_eroded"):
+        for file in files:
+            n = int((file.split('_')[2]).split('.')[0])
+            if n > total: total = n
+    #total = 1191
+    #total = int(input("npecas:"))
+
+    #total = 39
+    # modificado cuidado
+    frag_image = [cv2.imread('frag_eroded/frag_eroded_'+str(i)+'.png', 0)for i in range(0, total+1)]
+    #frag_image = [cv2.imread('frag_eroded/frag_eroded_'+str(i)+'.png', 0)for i in range(18, 20)]
+
+
+    paint = cv2.imread(target, 0)
+    paint_color = cv2.imread(target)
+    height, width = paint.shape
+
+    #n_cores = int(input("Cores:"))
+    n_cores = -1
+
+    if n_cores == -1: n_cores = multiprocessing.cpu_count()
+    list_id = [i for i in range(len(frag_image)) if frag_image[i] is not None]
+    all_id = list_id.copy()
+    total = len(list_id) #total validos
+
+
+
+    fragments_final = []
+    threadLock = threading.Lock()
+    #for metodo in [1]:
+    for metodo in [1,2,3,4,5,6,7,8,9,10]:
+        print("Layer",metodo)
+        paint_info = analise(paint, metodo)
+
+        threads = [None] * n_cores
+        fragments = [None] * n_cores
+
+        for i in range(len(threads)):
+            threads[i] = Thread(target=assembler, args=(frag_image, paint_info, list_id, metodo, fragments, i, threadLock))
+            threads[i].start()
+        for i in range(len(threads)): threads[i].join()
+
+        fragments = [y for x in fragments for y in x]
+
+        # filtro
+        '''
+        tmp = []
+        while(len(fragments) != 0): tmp.append(fragments.pop())
+        for frag in tmp:
+            if filtro(paint_color, frag):
+                fragments.append(frag)
+        del tmp
+        '''
+
+        for frag in fragments: all_id.remove(frag[5])
+        list_id = all_id.copy()
+
+        while(len(fragments) != 0): fragments_final.append(fragments.pop())
+
+        p = (total-len(list_id))/total
+        print('%',p)
+
+    #monta
+    #blank_image = np.zeros((len(paint),len(paint[0]),3), np.uint8)
+    blank_image = cv2.cvtColor(paint,cv2.COLOR_GRAY2RGB)
+    # aumento de brilho
+    blank_image = cv2.add(blank_image,np.array([50.0]))
+
+
+    log_file = open("log.txt","w")
+    with open("percent.txt","w") as file:
+        file.write(str(p)+"%")
+
+    seq = 0
+    get = []
+    fragments_final.sort(key=lambda x: x[5])
+    '''
+    frag [M, angle, tx, ty, r, i, dst_pts]
+    '''
+    for frag in fragments_final:
+
+        #FILTRO
+        '''
+        if not filtro(paint_color, frag):
+            print("Rejeita:", frag[5])
+            continue
+        '''
+
+        log_file.write(str(frag[5])+' '+str(frag[2])+' '+str(frag[3])+' '+str(frag[4])+'\n')
+        im = cv2.warpAffine(rotate(cv2.imread('frag_eroded/frag_eroded_'+str(frag[5])+'.png', -1), frag[1]), frag[0], (width, height))
+        blank_image = paste(blank_image, im, 0, 0)
+        cv2.imwrite('passos/frag_'+str(seq)+'_'+str(frag[5])+'.png', blank_image)
+        seq += 1
+        get.append(frag[5])
+
+    log_file.close()
+    print(seq, 'peças montadas')
+    print("Get:", get)
+
+    #result
+    cv2.imwrite('RECONSTRUCTED_fresco'+'.png', blank_image)
+
+
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
---
+++ tools.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  toolsCode.py
+#
+#  Copyright 2019 Thiago da Silva Teixeira <teixeira.zeus@gmail.com>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#
+
+import cv2
+import itertools as it
+import numpy as np
+
+def hist_comp(src_base, src_test):
+    hsv_base = cv2.cvtColor(src_base, cv2.COLOR_BGR2HSV)
+    hsv_test = cv2.cvtColor(src_test, cv2.COLOR_BGR2HSV)
+
+    h_bins = 50
+    s_bins = 60
+    histSize = [h_bins, s_bins]
+    h_ranges = [0, 180]
+    s_ranges = [0, 256]
+    ranges = h_ranges + s_ranges
+    channels = [0, 1]
+    hist_base = cv2.calcHist([hsv_base], channels, None, histSize, ranges, accumulate=False)
+    cv2.normalize(hist_base, hist_base, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
+
+    hist_test = cv2.calcHist([hsv_test], channels, None, histSize, ranges, accumulate=False)
+    cv2.normalize(hist_test, hist_test, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
+
+    return cv2.compareHist(hist_base, hist_test, 0)
+
+def dis(a,b):
+    return np.sqrt(abs(((b[0]-a[0])**2)+((b[1]-a[1])**2)))
+
+def baricentro(p):
+    x = (p[0][0]+p[1][0]+p[2][0])/3
+    y = (p[0][1]+p[1][1]+p[2][1])/3
+    return x,y
+
+def corta(img, pts):
+    pts = np.array([pts[0][0], pts[1][0], pts[2][0]])
+    xcentro, ycentro = baricentro(pts)
+    size = max(dis(pts[0],pts[1]),dis(pts[1],pts[2]),dis(pts[0],pts[2]))
+    origin = [(xcentro-(size/2)),(ycentro-(size/2))]
+    x = int(origin[0])
+    y = int(origin[1])
+    w = h = int(size)
+    return img[y:y+h, x:x+w].copy()
+
+def paste(base, cima, x_offset, y_offset):
+
+    y1, y2 = int(y_offset), int(y_offset + cima.shape[0])
+    x1, x2 = int(x_offset), int(x_offset + cima.shape[1])
+
+    alpha_s = cima[:, :, 3] / 255.0
+    alpha_l = 1.0 - alpha_s
+
+    for c in range(0, 3):
+        base[y1:y2, x1:x2, c] = (alpha_s * cima[:, :, c] + alpha_l * base[y1:y2, x1:x2, c])
+    return base
+
+def percent(falta):
+    count = 0
+    for i in range(len(falta)):
+        if falta[i] == False:
+            count += 1
+    return count/len(falta)
+
+def distance_dot(a,b):
+    return np.sqrt(abs(((b[1][0]-a[1][0])**2)+((b[1][1]-a[1][1])**2)))
+
+def tri_min(pontos, max_size):
+    if len(pontos) < 3 or max_size <= 0:
+        return []
+
+    # pega o menor triangulo nos pontos menor que max
+    tri_list = list(it.combinations(pontos, 3))
+    size = []
+    for tri in tri_list:
+        max = 0
+
+        for i,j in [[0,1],[1,2],[2,0]]:
+            d = distance_dot(tri[i],tri[j])
+            if d > max and not np.isnan(d):
+                max = d
+        if max <= max_size:
+            size.append([tri,max])
+    if len(size) == 0:
+        return []
+    #print(size)
+    matches = sorted(size, key=lambda x: x[1])
+    tri, value = matches[int(len(matches)/2)]
+    if value <= max_size:
+        return [x for x,y in tri]
+    else:
+        tri, value = matches[0]
+        if value <= max_size:
+            return [x for x,y in tri]
+
+    return []
+
+
+def rotate(image, angle):
+    if angle == 0: return image
+    if len(image.shape) > 2:
+        rows, cols, alpha = image.shape
+    else:
+        rows, cols = image.shape
+    M = cv2.getRotationMatrix2D((cols/2,rows/2), angle, 1)
+    dst = cv2.warpAffine(image, M, (cols,rows))
+    return dst
+
+
+def filtro(paint, frag):
+    try:
+        height, width, a = paint.shape
+
+        original = corta(paint, frag[6])
+
+        im = cv2.warpAffine(rotate(cv2.imread('frag_eroded/frag_eroded_'+str(frag[5])+'.png', -1), frag[1]), frag[0], (width, height))
+
+        modificado = paste(paint, im, 0, 0)
+        modificado = corta(modificado, frag[6])
+
+        rate = hist_comp(original, modificado)
+        print("H", frag[5], rate)
+        if rate > 0.4:
+            return True
+        else:
+            return False
+    except:
+        return True
+
+def analise(file, mode):
+    if mode == 1:
+        si = cv2.xfeatures2d.SIFT_create()
+        kp1, des1 = si.detectAndCompute(file, None)
+    elif mode == 2:
+        su = cv2.xfeatures2d.SURF_create(extended=0)
+        kp1, des1 = su.detectAndCompute(file, None)
+    elif mode == 3:
+        su = cv2.xfeatures2d.SURF_create(extended=1)
+        kp1, des1 = su.detectAndCompute(file, None)
+    elif mode == 4:
+        su = cv2.xfeatures2d.SURF_create(extended=0, nOctaves=1, hessianThreshold=100)
+        kp1, des1 = su.detectAndCompute(file, None)
+    elif mode == 5:
+        su = cv2.xfeatures2d.SURF_create(extended=1, nOctaves=1, hessianThreshold=100)
+        kp1, des1 = su.detectAndCompute(file, None)
+    elif mode == 6:
+        si = cv2.xfeatures2d.SIFT_create(nOctaveLayers=2,contrastThreshold=0.02, edgeThreshold=15)
+        kp1, des1 = si.detectAndCompute(file, None)
+    elif mode == 7:
+        si = cv2.xfeatures2d.SIFT_create(nOctaveLayers=2,contrastThreshold=0.01, edgeThreshold=25)
+        kp1, des1 = si.detectAndCompute(file, None)
+    elif mode == 8:
+        si = cv2.xfeatures2d.SIFT_create(nOctaveLayers=2,contrastThreshold=0.01, edgeThreshold=25, sigma=1)
+        kp1, des1 = si.detectAndCompute(file, None)
+    elif mode == 9:
+        si = cv2.xfeatures2d.SIFT_create(nOctaveLayers=2,contrastThreshold=0.01, edgeThreshold=30, sigma=0.8)
+        kp1, des1 = si.detectAndCompute(file, None)
+    elif mode == 10:
+        si = cv2.xfeatures2d.SIFT_create(nOctaveLayers=1,contrastThreshold=0.01, edgeThreshold=40, sigma=0.7)
+        kp1, des1 = si.detectAndCompute(file, None)
+
+    return [kp1, des1]
